ef3ec4f2a14ca53eb74ad8f0ad1d19f8935c1c28
thswlsqls <thswlsqls@naver.com>
2023-08-11 10:49:36 +0900

    first commit

---
+++ src/main/java/com/ebson/skillserver/SkillserverApplication.java
@@ -0,0 +1,13 @@
+package com.ebson.skillserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillserverApplication.class, args);
+	}
+
+}
