a9f220a765e03b9f8cfdaf72d0e298e00095bef6
eunbin son <58901024+thswlsqls@users.noreply.github.com>
2023-08-16 08:06:54 +0900

    commit changes

---
+++ src/main/java/com/ebson/skillserver/repository/TestRepository.java
@@ -0,0 +1,23 @@
+package com.ebson.skillserver.repository;
+
+import com.ebson.skillserver.domain.TestDomain;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Slf4j
+@RequiredArgsConstructor
+public class TestRepository {
+
+    @Autowired
+    private final EntityManager em;
+
+    public void save(TestDomain td){
+        log.info("TestRepository");
+        em.persist(td);
+    }
+
+}
---
+++ src/main/java/com/ebson/skillserver/service/TestService.java
@@ -0,0 +1,26 @@
+package com.ebson.skillserver.service;
+
+import com.ebson.skillserver.domain.TestDomain;
+import com.ebson.skillserver.repository.TestRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class TestService {
+
+    @Autowired
+    private final TestRepository testRepository;
+
+    public int insertTestDomain(TestDomain td){
+            log.info("TestService");
+            testRepository.save(td);
+            return 1;
+    }
+
+}
