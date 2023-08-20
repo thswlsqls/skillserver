0500ad57bcda8ef98eaac198cb89f4d0bf14e30c
thswlsqls <thswlsqls@naver.com>
2023-08-20 21:00:48 +0900

    TestUserControllerTest added

--- src/test/java/com/ebson/skillserver/controller/TestUserControllerTest.java
+++ src/test/java/com/ebson/skillserver/controller/TestUserControllerTest.java
@@ -1,14 +1,63 @@
 package com.ebson.skillserver.controller;
 
+import com.ebson.skillserver.domain.TestUser;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+import java.util.UUID;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class TestUserControllerTest {
 
     @Autowired
-    MockMvc mvc;
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/testUser/createTestUserForm 으로 get 요청한 결과 정상인지 테스트")
+    public void createTestUserFormTest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/testUser/createTestUserForm").accept(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @DisplayName("/testUser/retrieveAllTestUsers 으로 get 요청한 결과 정상인지 테스트")
+    public void retrieveAllTestUsersTest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/testUser/retrieveAllTestUsers").accept(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @DisplayName("/testUser/retrieveTestUserByUserIdForm 으로 get 요청한 결과 정상인지 테스트")
+    public void retrieveTestUserByUserIdFormTest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/testUser/retrieveTestUserByUserIdForm").accept(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @DisplayName("/testUser/createTestUser 으로 post 요청한 결과 정상인지 테스트")
+    @Transactional
+    public void createTestUserTest() throws Exception {
+        TestUser tu = new TestUser();
+        tu.setName(UUID.randomUUID().toString());
+        mockMvc.perform(MockMvcRequestBuilders.post("/testUser/createTestUser").requestAttr("testUser", tu))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+    }
+
+    @Test
+    @DisplayName("/testUser/retrieveTestUserByUserId 으로 post 요청한 결과 정상인지 테스트")
+    public void retrieveTestUserByUserId() throws Exception {
+        String userId = "319a4e2d8d6d4769b58082d4ea721fe7";
+        mockMvc.perform(MockMvcRequestBuilders.post("/testUser/createTestUser").requestAttr("userId", userId))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection());
+    }
 
 }
