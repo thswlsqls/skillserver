b518e33f49fdf87cd7c674215ae661a7d3b7c2ca
eunbin son <58901024+thswlsqls@users.noreply.github.com>
2023-08-23 18:17:50 +0900

    TestAopConfig.java modified

--- src/main/java/com/ebson/skillserver/aop/TestAopConfig.java
+++ src/main/java/com/ebson/skillserver/aop/TestAopConfig.java
@@ -2,8 +2,12 @@ package com.ebson.skillserver.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Slf4j
 @Aspect
@@ -36,14 +40,23 @@ public class TestAopConfig {
         log.info("postMapping finished ... ");
     }
 
-//    @AfterReturning(pointcut = "getMappingPointcut()", returning = "result")
-//    public void afterReturningGetMapping(Joinpoint joinpoint, Object result) throws Throwable {
-//        log.info("getMapping method returning - " + result);
-//    }
-//
-//    @AfterReturning(pointcut = "postMappingPointcut()", returning = "result")
-//    public void afterReturningPostMapping(Joinpoint joinpoint, Object result) throws Throwable {
-//        log.info("postMapping method returning - " + result);
-//    }
+    @Around("execution(* com.ebson.skillserver..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+        long start = System.currentTimeMillis();
+        Date startDate = new Date();
+        startDate.setTime(start);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS");
+
+        log.info(joinPoint.toString() + " - Start : " + simpleDateFormat.format(startDate));
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            Date finishDate = new Date();
+            finishDate.setTime(finish);
+            long timeMs = finish - start;
+            log.info(joinPoint.toString() + " - finish : " + simpleDateFormat.format(finishDate) + ", Elapsed time: "+ timeMs + "ms");
+        }
+    }
 
 }
