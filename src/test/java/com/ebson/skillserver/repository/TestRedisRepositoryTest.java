6f82c7337aadb554e0657c0ae7948a0dcc144cdd
thswlsqls <thswlsqls@naver.com>
2023-08-25 11:24:14 +0900

    TestRedisRepository and its test added

--- src/test/java/com/ebson/skillserver/repository/TestRedisRepositoryTest.java
+++ src/test/java/com/ebson/skillserver/repository/TestRedisRepositoryTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.UUID;
 
-@SpringBootTestgit
+@SpringBootTest
 public class TestRedisRepositoryTest {
 
     @Autowired
